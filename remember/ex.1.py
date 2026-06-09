2fcfdb118b8acccccc3fb37f68aa019d24610082
Kostya <beztalannyjkonstantin@gmail.com>
2026-06-09 13:01:23 +0000

    hard

--- remember/ex.1.py
+++ remember/ex.1.py
@@ -1,14 +1,19 @@
 import numpy as np
 
-sales = np.array([50, 10, 30, 20, 50, 10, 40, 30])
-
-u = np.unique(sales)
-print(u)
-print(u.size)
-
-print(np.argsort(sales))
-new_array = np.sort(sales)
-sales.sort()
-print(new_array)
-print(sales)
-print(sales[sales.argsort()])
+data = np.array(
+    [
+        [120, 80, 90, 100],
+        [60, 70, 65, 75],
+        [200, 180, 210, 190],
+        [50, 55, 60, 65],
+        [150, 160, 155, 165],
+    ]
+)
+avarage = data.mean(axis=1)
+print(avarage)
+print(avarage.argmax())
+print(data.argmax(axis=1))
+new_array = np.where(data < data.mean(), 0, data)
+print(data[(data > data.mean()) & (data < data.max())])
+data = data.reshape(data.size)
+print(data[data.argsort()])
