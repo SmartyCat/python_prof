195c4df8f318c602d136e2d16bba81e458cfc704
Kostya <beztalannyjkonstantin@gmail.com>
2026-06-09 12:18:36 +0000

    easy

--- remember/ex.1.py
+++ remember/ex.1.py
@@ -1,18 +1,14 @@
 import numpy as np
 
-sales = np.array(
-    [
-        [120, 150, 130, 170],
-        [90, 110, 105, 100],
-        [200, 180, 220, 210],
-        [70, 85, 80, 75],
-        [160, 155, 165, 170],
-    ]
-)
-middle_sale = sales.mean(axis=1)
-print(middle_sale)
-print(middle_sale.argmax())
-print(sales.argmax(axis=1))
-new_array = np.where(sales < sales.mean(),0,sales)
+sales = np.array([50, 10, 30, 20, 50, 10, 40, 30])
+
+u = np.unique(sales)
+print(u)
+print(u.size)
+
+print(np.argsort(sales))
+new_array = np.sort(sales)
+sales.sort()
 print(new_array)
-print(sales[(sales > sales.mean()) & (sales < 200)])
+print(sales)
+print(sales[sales.argsort()])
