a8639e73325771534afcca600cecab904a7e9024
Kostya <beztalannyjkonstantin@gmail.com>
2026-06-09 11:56:40 +0000

    easy

--- remember/ex.1.py
+++ remember/ex.1.py
@@ -1,14 +1,19 @@
 import numpy as np
 
-results = np.array(
-    [[12, 15, 14, 16], [18, 17, 19, 20], [10, 11, 13, 12], [21, 22, 20, 23]]
+scores = np.array(
+    [
+        [72, 85, 90, 88],
+        [60, 58, 65, 70],
+        [95, 92, 96, 98],
+        [40, 55, 50, 45],
+        [80, 82, 84, 86],
+    ]
 )
-avar_every_athlet = results.mean(axis=1)
-print(avar_every_athlet)
-max_index = np.where(results.mean(axis=1) == avar_every_athlet.max())
-print(*max_index)
-best_chance = results.argmax(axis=1)
-print(best_chance)
-new_results = np.where(results < results.mean(), 0, results)
-print(new_results)
-print(results[(results > 15) & (results < 22)])
+
+middle_score = scores.mean(axis=1)
+print(middle_score)
+print(np.where(scores.mean(axis=1) > scores.mean())[0])
+print(scores.argmax(axis=0))
+new_array = np.where(scores < 60, 60, scores)
+print(new_array)
+print(scores[(scores >= 80) & (scores <= 95)])
