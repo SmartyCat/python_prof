c0dcbff6c3c48deb2550ff01340bf881dfc5107d
Kostya <beztalannyjkonstantin@gmail.com>
2026-06-10 06:10:20 +0000

    really easy

---
+++ time_this_is_time/ex1.py
@@ -0,0 +1,17 @@
+"""Дополните приведенный ниже код, чтобы он вывел текущую дату в ISO формате (YYYY-MM-DD).
+
+Напишите программу. Тестируется через stdin → stdout
+
+Time Limit: 15 секунд
+Memory Limit: 256 MB
+
+# импортируем тип date из модуля datetime
+from ____ import ____
+
+# выводим текущую дату
+print(____)
+"""
+
+from datetime import date
+
+print(date.today())
